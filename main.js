b9e9245fc57ae720760232a29708d7eb36e2a0f5
Saman Givian <samgivian2015@gmail.com>
2021-01-22 19:58:10 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 const port = 3000
 var path = require('path');
 
+const dbuRI = "mongodb+srv://netninda:1234test@cluster0.svjrl.mongodb.net/note-tuts?retryWrites=true&w=majority"
+mongoose.connect(dbuRI, { useNewUrlParser: true, useUnifiedTopology: true }).then((result) =>
+  console.log('connected to db')).catch((error) => console.log(error))
+const db = mongoose.connection;
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  snippet: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
+const Blog = mongoose.model('Blog', blogSchema);
+
+
+
+
 app.get('/', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog',
+  });
+  blog.save();
+  Blog.find({title:'new blog'}).then((results)=>{
+    console.log(results);
+  })
+  Blog.findOne({ title: 'new blog' }, function (err, doc){
+    doc.title = 'jason bourne';
+    doc.save();
+  });
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.get('/map', (req, res) => {
